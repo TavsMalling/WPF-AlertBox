c3334d60c80356dad3742bd5c22063b7a3567060
TavsMalling <67231672+TavsMalling@users.noreply.github.com>
2020-07-03 08:02:38 +0200

    Main upload

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class MainWindow
+    Private Sub Enter(sender As Object, e As KeyEventArgs)
+        If (e.Key = Key.Return) Then
+            MessageBox.Show(alertBox.Text, "Alert!", MessageBoxButton.OKCancel)
+            e.Handled = True
+        End If
+
+
+
+    End Sub
+End Class
